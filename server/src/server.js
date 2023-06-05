038e37386fc4969f77a80fb676e203f2ddfbceb3
maryambani <119344521+maryambani@users.noreply.github.com>
2023-06-05 11:21:48 -0400

    added mongo database

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const mongoose = require('mongoose')
 
 const app = require('./app');
 
@@ -6,9 +7,20 @@ const { loadPlanetsData } = require ('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL = 'mongodb+srv://nasa-api:cAd2t4UoF3mJwT1Q@cluster0.lqwjp3m.mongodb.net/nasa?retryWrites=true&w=majority';
+
 const server = http.createServer(app);
 
+mongoose.connection.once('open', () => {
+    console.log('MongoDB connection ready!');
+});
+
+mongoose.connection.on('error', (err) => {
+    console.error(err)
+});
+
 async function startServer() {
+    await mongoose.connect(MONGO_URL);
     await loadPlanetsData();
 
     server.listen(PORT, () => {
